8c7320e83a2c55f55141b21ef07f8f278730c3ff
Laura Chihara <lchihara@carleton.edu>
2015-12-15 20:48:18 +0000

    version 1.2

--- R/corDemo.R
+++ R/corDemo.R
@@ -1,25 +1,26 @@
 corDemo <-
 function(r = 0)
 {
-    #choose different "r" values by clicking on the vertical strip on the
-    #right-hand side. Press ESC to exit
-    require(MASS)
+
+    if (requireNamespace("MASS", quietly = TRUE)){
     cat("Input r. To quit, type r = 9\n")
      while(abs(r) <= 1)
        {
         sigma <- cbind(c(1,r), c(r, 1))
-       
-        temp <- mvrnorm(100, mu = c(0,0),Sigma = sigma)
-          
+
+        temp <- MASS::mvrnorm(100, mu = c(0,0),Sigma = sigma)
+
          plot(temp, xlab = "x", ylab = "y", main = paste(
             "Correlation =", round(r, 2)), xlim = c(-3, 3), ylim = c(-3, 3))
-               
+
         r <- readline("r = ")
-        r <- eval(parse(text=r))     
-        
-        
-        }     
-    
-    detach(package:MASS)      
+        r <- eval(parse(text=r))
+
+
+        }
+
     invisible()
+}
+
+else {stop("Install the package MASS")}
 }
--- R/permTest.default.R
+++ R/permTest.default.R
@@ -1,28 +1,27 @@
-permTest.default <-
-function(x, group, fun = mean,   B = 9999, 
+permTest.default <- function(x, group, fun = mean,   B = 9999,
      alternative="two.sided", plot.hist = TRUE, legend.loc = "topright", plot.qq=FALSE, ...)
-    {  
-       
+    {
+
        if (is.character(fun)){
         fun.name <- fun
-        fun <- eval(parse(text=fun))  
+        fun <- eval(parse(text=fun))
         } else  fun.name <- deparse(substitute(fun))
 
-   
-      if (!is.numeric(x)) stop("Variable must be numeric.")           
+
+      if (!is.numeric(x)) stop("Variable must be numeric.")
       if (is.factor(group)) {
          group <- droplevels(group)
          } else group <- as.factor(group)
       if (length(levels(group)) != 2) stop("Grouping variable must have only two levels")
-      
+
       comCases <- complete.cases(x, group)
       nmiss <- length(x) - sum(comCases)
        if (nmiss > 0)
         cat("\n ", nmiss, "observations removed due to missing values.\n")
-      
+
       x <- x[comCases]
       group <- group[comCases]
-    
+
     group1 <- x[group==levels(group)[1]]
     group2 <- x[group==levels(group)[2]]
     group1.name <- levels(group)[1]
@@ -34,19 +33,19 @@ function(x, group, fun = mean,   B = 9999,
     m <- length(group1)
 
     observed <- fun(group1) - fun(group2)
- 
+
     result <- numeric(B)
     for (i in 1:B)
     {
        index <- sample(n, m, replace=FALSE)
-       result[i] <- fun(x[index]) - fun(x[-index])    
+       result[i] <- fun(x[index]) - fun(x[-index])
      }  #end for
 
   mean.PermDist <- round(mean(result), 5)
   sd.PermDist <- round(sd(result), 5)
-  
+
   alt <- pmatch(alternative, c("less", "greater", "two.sided"), nomatch=4)
-  
+
    P <- c((sum(result <= observed) + 1)/(B + 1), (sum(result >= observed) + 1)/(B + 1))
 
    P.value <- switch(alt,
@@ -55,19 +54,19 @@ function(x, group, fun = mean,   B = 9999,
          2*min(P[1],P[2]),
          stop("Alternative not matched.")
   )
-    
+
   my.title <- paste("Permutation distribution", fun.name, ":\n" ,group1.name, "-", group2.name, sep= " ")
   out <- hist(result, plot = F)
   out$density <- 100*out$counts/sum(out$counts)
   xrange <- range(c(out$breaks, observed))
-  plot(out, freq = FALSE, ,xlim = xrange, main=my.title, ylab="Percent", cex.main=.9, 
+  plot(out, freq = FALSE, ,xlim = xrange, main=my.title, ylab="Percent", cex.main=.9,
   xlab = "Differences", cex.lab = .9)
-  
+
    points(observed, 0, pch = 2, col = "red")
    points(mean.PermDist, 0, pch = 8, col = "blue")
    legend(legend.loc, legend = c("Observed", "Permutation"), pch = c(2, 8), col= c("red", "blue"),
     cex = .9)
- 
+
 
   cat("\n\t** Permutation test **\n")
   cat("\n Permutation test with alternative:", alternative,"\n")
